1c19793e57d81c491b69f40c9a925f36fb76c790
0xIbuki <junta8918@gmail.com>
2023-04-15 09:11:26 +0900

    deply and test script for attester

---
+++ packages/hardhat/deploy/01_deploy_workerAsserter.ts
@@ -0,0 +1,46 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+
+/**
+ * Deploys a contract named "YourContract" using the deployer account and
+ * constructor arguments set to the deployer address
+ *
+ * @param hre HardhatRuntimeEnvironment object.
+ */
+const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  /*
+    On localhost, the deployer account is the one that comes with Hardhat, which is already funded.
+
+    When deploying to live networks (e.g `yarn deploy --network goerli`), the deployer account
+    should have sufficient balance to pay for the gas fees for contract creation.
+
+    You can generate a random account with `yarn generate` which will fill DEPLOYER_PRIVATE_KEY
+    with a random private key in the .env file (then used on hardhat.config.ts)
+    You can run the `yarn account` command to check your balance in every network.
+  */
+  const { deployer } = await hre.getNamedAccounts();
+  const { deploy } = hre.deployments;
+  // const _defaultCurrency = hre.ethers.constants.AddressZero;
+  const _defaultCurrency = "0xd35cceead182dcee0f148ebac9447da2c4d449c4";
+  const _optimisticOracleV3 = "0x9923D42eF695B5dd9911D05Ac944d4cAca3c4EAB";
+  // const _optimisticOracleV3 = ""; // mumbai
+
+  await deploy("DataAttester", {
+    from: deployer,
+    // Contract constructor arguments
+    args: [_defaultCurrency, _optimisticOracleV3],
+    log: true,
+    // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
+    // automatically mining the contract deployment transaction. There is no effect on live networks.
+    autoMine: true,
+  });
+
+  // Get the deployed contract
+  // const yourContract = await hre.ethers.getContract("YourContract", deployer);
+};
+
+export default deployYourContract;
+
+// Tags are useful if you have multiple deploy files and only want to run one of them.
+// e.g. yarn deploy --tags YourContract
+deployYourContract.tags = ["DataAsserter"];
---
+++ packages/hardhat/test/deleteFlow.ts
@@ -0,0 +1,48 @@
+import { MoneyRouter } from "./../typechain-types/contracts/MoneyRouter";
+import { Result } from "@ethersproject/abi";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { DataAttester } from "../typechain-types";
+
+import { time } from "@nomicfoundation/hardhat-network-helpers";
+
+const { Framework } = require("@superfluid-finance/sdk-core");
+
+describe.only("deleteFlow", function () {
+  // We define a fixture to reuse the same setup in every test.
+  const _defaultCurrency = "0xd35cceead182dcee0f148ebac9447da2c4d449c4";
+  const _optimisticOracleV3 = "0x9923D42eF695B5dd9911D05Ac944d4cAca3c4EAB";
+
+  let DataAttester: DataAttester;
+  let MoneyRouter: MoneyRouter;
+  before(async () => {
+    const DataAttesterContractFactory = await ethers.getContractFactory("DataAttester");
+    DataAttester = (await DataAttesterContractFactory.deploy(_defaultCurrency, _optimisticOracleV3)) as DataAttester;
+    await DataAttester.deployed();
+
+    const MoneyRouterContractFactory = await ethers.getContractFactory("MoneyRouter");
+    MoneyRouter = (await MoneyRouterContractFactory.deploy(
+      ethers.constants.AddressZero,
+      DataAttester.address,
+    )) as MoneyRouter;
+    await DataAttester.deployed();
+  });
+
+  describe("Functions", function () {
+    it("Should delete flow", async function () {
+      const provider = new ethers.providers.JsonRpcProvider(
+        "https://goerli.infura.io/v3/ce7426bf07f24fd59a2f7bbb6df217b4",
+      );
+
+      const sf = await Framework.create({
+        chainId: (await provider.getNetwork()).chainId,
+        provider,
+      });
+      const daix = await sf.loadSuperToken("fDAIx");
+
+      const receiver = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+
+      const result = await MoneyRouter.createFlowFromContract(daix.address, receiver, "85802469135802", "43243");
+    });
+  });
+});
