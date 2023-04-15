7fb7ee06c61aee461d7217a41a4e0c303cc7cfad
Shiv Bhonde <shivbhonde04@gmail.com>
2023-04-16 00:01:57 +0900

    add dynamic address to pages

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,14 +1,31 @@
-import { useContext, useState } from "react";
 import Head from "next/head";
 import Link from "next/link";
-import { TaskContext } from "./TaskContext";
+import { gql, useQuery } from "@apollo/client";
+import { ethers } from "ethers";
 import type { NextPage } from "next";
-import { Status } from "~~/components/Status";
-import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
-import scaffoldConfig from "~~/scaffold.config";
+import { Spinner } from "~~/components/Spinner";
+import { Address } from "~~/components/scaffold-eth";
+
+export const GET_STREAMS = gql`
+  query MyQuery($currentFlowRate_gt: BigInt = "0") {
+    streams(where: { sender: "0x26439c143fff24b4c9114ef77f568b596613078f", currentFlowRate_gt: $currentFlowRate_gt }) {
+      currentFlowRate
+      token {
+        symbol
+      }
+      receiver {
+        id
+      }
+      sender {
+        id
+      }
+    }
+  }
+`;
 
 const Home: NextPage = () => {
-  const tasks = useContext(TaskContext);
+  const { loading: loadingAllStreams, data: allStreamsData } = useQuery(GET_STREAMS);
+  console.log("⚡️ ~ file: superfluid.tsx:42 ~ data:", allStreamsData);
 
   return (
     <>
@@ -16,26 +33,46 @@ const Home: NextPage = () => {
         <title>Fluidpay App</title>
         <meta name="description" content="Created with 🏗 scaffold-eth" />
       </Head>
-
-      <div className="flex flex-col space-y-2 m-5"></div>
-      {tasks?.tasks?.map(task => (
-        <div className="card w-120 bg-base-100 shadow-xl" key={task.id}>
-          <div className="card-body">
-            <Link href="/detail">
-              <h2 className="card-title mb-0 mr-10">{task.worker}</h2>
-            </Link>
-            <div className="flex justify-between items-center">
-              <Status speed={task.speed} amount={task.amount} />
+      <div className="flex flex-col justify-center items-center spacey-y-6">
+        <h1 className="text-center text-3xl">Builders</h1>
+        <div className="overflow-x-auto w-[50%]">
+          {loadingAllStreams ? (
+            <div className="mx-auto">
+              <Spinner width="50" height="50" />
             </div>
-
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">
-                <Link href="/detail">Detail</Link>
-              </button>
-            </div>
-          </div>
+          ) : allStreamsData?.streams?.length > 0 ? (
+            <table className="table w-full">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th>From Address</th>
+                  <th>To Address</th>
+                  <th>Current Stream / sec</th>
+                </tr>
+              </thead>
+              <tbody>
+                {allStreamsData.streams.map(stream => {
+                  return (
+                    <tr key={stream.receiver.id}>
+                      <td>
+                        <Address address={stream.sender.id} />
+                      </td>
+                      <td>
+                        <Link href={`/builder/${stream.receiver.id}`}>
+                          <Address address={stream.receiver.id} />
+                        </Link>
+                      </td>
+                      <td>{ethers.utils.formatEther(stream.currentFlowRate)}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          ) : (
+            <div>No active stream found</div>
+          )}
         </div>
-      ))}
+      </div>
     </>
   );
 };
