40022302ebcaafb3ca5c036144a0d501cfc15a00
YIHAN <105974692+neil0x46dc@users.noreply.github.com>
2023-04-15 20:57:32 +0900

    feat: task

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -3,6 +3,36 @@ import Link from "next/link";
 import type { NextPage } from "next";
 import { Status } from "~~/components/Status";
 
+interface Task {
+  id: number;
+  title: string;
+  description?: string;
+  speed: string;
+  amount: string;
+}
+
+const tasks: Task[] = [
+  {
+    id: 1,
+    title: "Complete TypeScript tutorial",
+    description: "Learn how to use TypeScript with Node.js and React",
+    speed: "0.05",
+    amount: "1",
+  },
+  {
+    id: 2,
+    title: "Finish reading 'The Alchemist'",
+    speed: "0.04",
+    amount: "2",
+  },
+  {
+    id: 3,
+    title: "Clean the house",
+    speed: "0.03",
+    amount: "0.5",
+  },
+];
+
 const Home: NextPage = () => {
   return (
     <>
@@ -12,23 +42,25 @@ const Home: NextPage = () => {
       </Head>
 
       <div className="flex flex-col space-y-2 m-5">
-        <div className="card w-120 bg-base-100 shadow-xl">
-          <div className="card-body">
-            <div className="flex justify-between items-center">
-              <h2 className="card-title mb-0 mr-10">Task1</h2>
-              <Status speed="0.5eth/min" amount="1.5" />
-            </div>
-            <p>description description description</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">
-                <Link href="/submit">Submit Progress</Link>
-              </button>
-              <button className="btn btn-primary">
-                <Link href="/challengepage">Challenge</Link>
-              </button>
+        {tasks.map(task => (
+          <div className="card w-120 bg-base-100 shadow-xl" key={task.id}>
+            <div className="card-body">
+              <div className="flex justify-between items-center">
+                <h2 className="card-title mb-0 mr-10">{task.title}</h2>
+                <Status speed={task.speed} amount={task.amount} />
+              </div>
+              <p>{task.description}</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">
+                  <Link href="/submit">Submit Progress</Link>
+                </button>
+                <button className="btn btn-primary">
+                  <Link href="/challengepage">Challenge</Link>
+                </button>
+              </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
     </>
   );
