f3098bdc357c5cfd5c3351574f90f881458e4541
0xIbuki <junta8918@gmail.com>
2023-04-15 21:43:13 +0900

    merge index

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,43 +1,17 @@
 import { useState } from "react";
+import { useContext } from "react";
 import Head from "next/head";
 import Link from "next/link";
+import { TaskContext } from "./TaskContext";
 import { ethers } from "ethers";
 import type { NextPage } from "next";
 import { Status } from "~~/components/Status";
 import { useDeployedContractInfo, useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
 import scaffoldConfig from "~~/scaffold.config";
 
-interface Task {
-  id: number;
-  title: string;
-  description?: string;
-  speed: string;
-  amount: string;
-}
-
-const tasks: Task[] = [
-  {
-    id: 1,
-    title: "Complete TypeScript tutorial",
-    description: "Learn how to use TypeScript with Node.js and React",
-    speed: "0.05",
-    amount: "1",
-  },
-  {
-    id: 2,
-    title: "Finish reading 'The Alchemist'",
-    speed: "0.04",
-    amount: "2",
-  },
-  {
-    id: 3,
-    title: "Clean the house",
-    speed: "0.03",
-    amount: "0.5",
-  },
-];
-
 const Home: NextPage = () => {
+  const tasks = useContext(TaskContext);
+
   const [doneClaim, setDoneClaim] = useState(false);
 
   const statement = "0xIBuki.eth has stopped sumibtting working progress from April 1st, 2023.";
@@ -71,7 +45,7 @@ const Home: NextPage = () => {
       </Head>
 
       <div className="flex flex-col space-y-2 m-5"></div>
-      {tasks.map(task => (
+      {tasks?.tasks?.map(task => (
         <div className="card w-120 bg-base-100 shadow-xl" key={task.id}>
           <div className="card-body">
             <div className="flex justify-between items-center">
